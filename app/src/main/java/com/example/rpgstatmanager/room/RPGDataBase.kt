d7a82c9ef1d0b0bbc3344ffb070281b63b579c54
hollo007 <akos.hollo-szabo@qlmdc.hu>
2020-05-09 17:09:55 +0200

    daos connected to database

--- app/src/main/java/com/example/rpgstatmanager/room/RPGDataBase.kt
+++ app/src/main/java/com/example/rpgstatmanager/room/RPGDataBase.kt
@@ -4,12 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.rpgstatmanager.room.dao.DAO_Ability
+import com.example.rpgstatmanager.room.dao.*
 import com.example.rpgstatmanager.room.data.*
 
 @Database(
-    entities = arrayOf(
+    entities = [
         E_Ability::class,
+        E_AbilityMoveConnector::class,
         E_Adventure::class,
         E_Character::class,
         E_EmotionModifier::class,
@@ -20,12 +21,28 @@ import com.example.rpgstatmanager.room.data.*
         E_Race::class,
         E_Stat::class,
         E_Tool::class,
-        E_Weapon::class
-    ), version = 1
+        E_ToolMoveConnector::class,
+        E_Weapon::class,
+        E_WeaponMoveConnector::class
+    ], version = 1
 )
 abstract class RPGDatabase : RoomDatabase() {
 
     abstract fun abilityDao(): DAO_Ability
+    abstract fun abilityMoveConnectorDao(): DAO_AbilityMoveConnector
+    abstract fun adventureDao(): DAO_Adventure
+    abstract fun characterDao(): DAO_Character
+    abstract fun emotionModifierDao(): DAO_EmotionModifier
+    abstract fun keyValueDao(): DAO_KeyValue
+    abstract fun moveDao(): DAO_Move
+    abstract fun moveTypeDao(): DAO_MoveType
+    abstract fun personalityTypeDao(): DAO_PersionalityType
+    abstract fun raceDao(): DAO_Race
+    abstract fun statDao(): DAO_Stat
+    abstract fun toolDao(): DAO_Tool
+    abstract fun toolMoveConnectorDao(): DAO_ToolMoveConnector
+    abstract fun weaponDao(): DAO_Weapon
+    abstract fun weaponMoveConnectorDao(): DAO_WeaponMoveConnector
 
     companion object {
         private var INSTANCE: RPGDatabase? = null
